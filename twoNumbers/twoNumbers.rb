bb5a7faa810874e579331a073d984c925d4468e8
sepideh <sepideh.siadatan@gmail.com>
2018-10-18 23:44:59 +0800

    Add TwoNumbers

---
+++ twoNumbers/twoNumbers.rb
@@ -0,0 +1,13 @@
+puts "write any number:"
+print "number1: "
+number1 = gets.chomp.to_i
+print "number2: "
+number2 =gets.chomp.to_i
+
+answer = number1 + number2 
+
+print number1.to_s + " + " + number2.to_s + " = "
+
+puts answer 
+
+
