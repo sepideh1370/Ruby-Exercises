8c6f3a3fa135b071732e9de6e7c4159dbda73dd7
sepideh <sepideh.siadatan@gmail.com>
2018-10-23 10:37:53 +0800

    Add WordReverse

---
+++ wordReverse/wordReverse.rb
@@ -0,0 +1,9 @@
+puts "Enter any sentence with only 4 words:"
+word1 = gets.chomp
+word2 = gets.chomp
+word3 = gets.chomp
+word4 = gets.chomp
+
+puts word1  + " "+ word2   + " " +   word3   + " " +  word4
+
+puts word4  + " "+ word3   + " " +   word2   + " " +  word1
